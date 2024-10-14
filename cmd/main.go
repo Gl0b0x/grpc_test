0f798d58c19d3174539584568f45a6771b453c8f
Gl0b0x <nepisamnte80@gmail.com>
2024-10-14 16:11:34 +0300

    create main and method GET

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,41 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	desc "github.com/Gl0b0x/grpc_test/pkg/user_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
 )
 
 const grpcPort = 50051
 
 type server struct {
 	desc.UnimplementedUserV1Server
+}
+
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("User id: %d", req.GetId())
+	return &desc.GetResponse{
+		Id:    req.GetId(),
+		Name:  "Globox",
+		Email: "vanyakyz@mail.ru",
+		Role:  desc.Role_user,
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterUserV1Server(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
